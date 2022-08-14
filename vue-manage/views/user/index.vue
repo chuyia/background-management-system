c8fc08740e9fc96721e1bfd4e74a726458caec13
Leexinxian <chuyia1999@163.com>
2022-08-14 15:12:13 +0800

    表单

--- vue-manage/views/user/index.vue
+++ vue-manage/views/user/index.vue
@@ -1,17 +1,26 @@
 <template>
-    <div>
-        我是user
+    <div class="manage">
+        <el-dialog :title="operateType === 'add' ? '新增用户' : '更新用户'" :visible.sync="isShow">
+            <common-form>
+
+                
+            </common-form>
+        </el-dialog>
     </div>
 </template>
 
 <script>
-
+import CommonForm from '@/components/CommonForm.vue'
 export default {
     name: 'user',
-    components: {},
+    components: {
+        CommonForm
+    },
     props: {},
     data() {
         return {
+            operateType: 'add',
+            isShow: false
         };
     },
     watch: {},
